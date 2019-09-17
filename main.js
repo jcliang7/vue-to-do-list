887579d2abb6360f34bfc47343edb1a0d24d4c5e
Jia-Chi Liang NB-Win10 <dachurita@gmail.com>
2019-09-17 17:54:56 +0800

    finish 0917

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ new Vue({
     el: "#app",
     data: {
         message: 'Hello, I have to do these things today.',
+        inputValue: null,
         todos: [
             { text: "Modify the PPT of Lispeak.", done: false },
             { text: "Test Coding-pass", done: false },
@@ -14,6 +15,14 @@ new Vue({
             console.log("Beofre:", todo.done)
             todo.done = !todo.done
             console.log("After:", todo.done)
+        },
+
+        add: function () {
+            console.log(this.inputValue)
+            this.todos.push({ text: this.inputValue, done:false });
+            this.inputValue = "";
+            console.log("Why the word not show?")
         }
+
     }
 })
